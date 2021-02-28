fa413383716948e1b3e48cb3bed7a8c90f8725ec
Ahmed <ahmedrassoulahmed@gmail.com>
2021-02-28 11:40:55 +0100

    Swiper Slide

--- main.js
+++ main.js
@@ -10,3 +10,31 @@ new Masonry("#post .grid", {
     itemSelector : '.grid-item',
     gutter : 20
 });
+
+// swiper library initialization
+new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 5,
+    autoplay: {
+        delay: 3000
+    },
+    // responsive breakpoints
+    breakpoints: {
+        '@0': {
+            slidesPerView: 2
+        },
+        // greater than 888px
+        '@1.00': {
+            slidesPerView: 3
+        },
+        // greater than 1110px
+        '@1.25': {
+            slidesPerView: 4
+        },
+        // 1330px
+        '@1.50': {
+            slidesPerView: 5
+        }
+    }
+});
